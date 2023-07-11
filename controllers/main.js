44fe431f87cd4f631bfb0e3be7937a6fe1506d59
Prafull Epili <prafullepili@gmail.com>
2023-07-11 16:41:31 +0530

    token creation done

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,18 @@
-const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req,res) => {
     const {username, password } = req.body;
     if(!username || !password){
         throw new CustomAPIError("Please provide email and password", 400)
     }
-    res.send("Fake Login/Register/Signup Route")
+     
+    const id = new Date().getDate();
+    const token = jwt.sign({id,username}, process.env.JWT_SECRET, {
+        expiresIn :'30d'
+    })
+
+    res.status(200).json({msg: 'user created',token});
 }
 
 const dashboard = async (req, res) => {
